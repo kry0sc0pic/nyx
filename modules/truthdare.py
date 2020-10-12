6abb1397dab6d05d11fa4dd88ad14e8e06ae84ea
Krishaay Jois <krishaayjois@pop-os.localdomain>
2020-10-12 15:06:30 +0530

    Added TODO in truthdare extension

--- modules/truthdare.py
+++ modules/truthdare.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 class TruthDare(commands.Cog):
     def __init__(self, client):
         self.client = client
+        # TODO Load truths and dares from json file
 
     @commands.command()
     async def truth(self, ctx):
