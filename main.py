f217913882484fcc55851700bba9eafe696d5c41
Krishaay Jois <krishaayjois@pop-os.localdomain>
2020-10-12 23:03:04 +0530

    Added code to load  variables

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 import json
 import os
 from discord.ext import commands
+# from dotenv import load_dotenv
+# load_dotenv()
 client = commands.Bot(command_prefix="$")
 status = discord.Status.online
 act = discord.Activity(type=discord.ActivityType.listening, name="to !help")
@@ -20,6 +22,5 @@ for cog in config["load_cogs"]:
     client.load_extension(cog)
 
 
-token = "<token>"  # For local testing
-# token = os.environ.get("TOKEN") # For heroku deployment
+token = os.environ.get("TOKEN")  # For heroku deployment
 client.run(token)
