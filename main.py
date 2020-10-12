740e1d8737f5904119a642a0d58f24ac2f5eeeec
Krishaay Jois <krishaayjois@pop-os.localdomain>
2020-10-12 14:51:49 +0530

    extension loading implemented

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import discord
-from discord import activity
+import json
+import os
 from discord.ext import commands
-client = commands.Bot(command_prefix="!")
+client = commands.Bot(command_prefix="$")
 status = discord.Status.online
 act = discord.Activity(type=discord.ActivityType.listening, name="to !help")
+with open("config/config.json") as jsonconfig:
+    config = json.load(jsonconfig)
 
 
 @client.event
@@ -11,4 +14,11 @@ async def on_ready():
     await client.change_presence(status=status, activity=act)
     print("Bot is Ready")
 
-# client.run(token)
+
+for cog in config["load_cogs"]:
+
+    client.load_extension(cog)
+
+
+# token = "<token>" # For local testing
+# token = os.environ.get("TOKEN") # For heroku deployment
