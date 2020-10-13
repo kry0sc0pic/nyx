4256be073f9e49eac232dc7dc1d1eefb7ff42f4d
Krishaay Jois <krishaayjois@pop-os.localdomain>
2020-10-13 14:19:40 +0530

    Added files for load,unload and reload

---
+++ modules/cogfunctions.py
@@ -0,0 +1,27 @@
+from discord.ext import commands
+from discord.ext.commands.core import command
+
+
+class CogFunctions(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def disable(self, ctx, module):
+        self.client.unload_extension("modules."+module.strip())
+        await ctx.send(f"Disabled module: `{module}`")
+
+    @commands.command()
+    async def enable(self, ctx, module):
+        self.client.load_extension("modules."+module.strip())
+        await ctx.send(f"Enabled module: `{module.strip()}`")
+
+    @commands.command()
+    async def renable(self, ctx, module):
+        self.client.unload_extension("modules."+module.strip())
+        self.client.load_extension("modules."+module.strip())
+        await ctx.send(f"Renabled module: `{module.strip}`")
+
+
+def setup(client):
+    client.add_cog(CogFunctions(client))
