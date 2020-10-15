7cd9383c0caffd195347c8a6e27fd4e0de1ad65e
Krishaay Jois <krishaayjois@pop-os.localdomain>
2020-10-15 14:32:29 +0530

    Help Module reads data from external file

--- modules/help.py
+++ modules/help.py
@@ -2,6 +2,12 @@ from discord import colour
 from discord import embeds
 from discord.ext import commands
 import discord
+import json
+
+with open("assets/json/help.json", "r") as f:
+    helpdata = json.load(f)
+    thumb = helpdata["thumbnail"]
+    helpdata = helpdata["data"]
 
 
 class Help(commands.Cog):
@@ -12,19 +18,10 @@ class Help(commands.Cog):
     async def help(self, ctx):
         helpEmbed = discord.Embed(
             title="Bot Help", description="Help for Nyx Bot", colour=0xee6f57)
-        helpEmbed.add_field(
-            name="$help", value="Displays the help for the bot", inline=False)
-        helpEmbed.add_field(
-            name="$truth or $t", value="Sends a random truth question")
-        helpEmbed.add_field(
-            name="$dare or $d", value="Sends a random dare")
-        helpEmbed.add_field(
-            name="$never or $n", value="Sends a never have I ever question")
-        helpEmbed.add_field(
-            name="$wanted", value="Puts you avatar on a wanted poster", inline=False)
-        helpEmbed.add_field(name="Github Repo",
-                            value="https://github.com/krishaayjois21/nyx", inline=False)
-        helpEmbed.set_thumbnail(url="https://i.imgur.com/fCZVySr.png")
+        for field in helpdata:
+            helpEmbed.add_field(
+                name=field["name"], value=field["value"], inline=field["inline"])
+        helpEmbed.set_thumbnail(url=thumb)
 
         helpEmbed.set_footer(
             text="Like the bot? , Consider giving it a star on github 🙂")
